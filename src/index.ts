9a9260df563ca3f17b8179362e5bad1211b009d5
Alan Cezar <alan.cezar.silva@gmail.com>
2024-11-10 20:20:58 -0300

    testando imagem no minio

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import Fastify from 'fastify'
-// import * as Minio from 'minio'
+import * as Minio from 'minio'
 import { sendMediaToEvolutionApi, sendToEvolutionApi, setEvolutionPayload } from './evolution-api/evolution.service'
 import { IEvolutionPayload, MessageTypeEnum } from './evolution-api/evolution.model'
 import {
@@ -12,36 +12,36 @@ const fastify = Fastify({
   logger: true
 })
 
-// const minioClient = new Minio.Client({
-//   endPoint: 's3.i4ai.com.br',
-//   useSSL: true,
-//   accessKey: '1IS4rz3JGlBV0uuQYfAb',
-//   secretKey: 'YLVI6tBIrF5pVukAnOpxhe8eysb84UAk4VuMFMgB',
-// });
+const minioClient = new Minio.Client({
+  endPoint: 's3.i4ai.com.br',
+  useSSL: true,
+  accessKey: '1IS4rz3JGlBV0uuQYfAb',
+  secretKey: 'YLVI6tBIrF5pVukAnOpxhe8eysb84UAk4VuMFMgB',
+});
 
-// async function uploadToMinIO(bucketName: string, objectName: string, buffer: Buffer, mediaType: string) {
-//   try {
-//     const bucketExists = await minioClient.bucketExists('router');
+async function uploadToMinIO(bucketName: string, objectName: string, buffer: Buffer, mediaType: string) {
+  try {
+    const bucketExists = await minioClient.bucketExists('router');
 
-//     if (!bucketExists) {
-//       await minioClient.makeBucket('router', 'us-east-1');
-//     }
+    if (!bucketExists) {
+      await minioClient.makeBucket('router', 'us-east-1');
+    }
 
-//     await minioClient.putObject('router', bucketName + '-' + objectName, buffer, buffer.length, {
-//       'Content-Type': mediaType,
-//     });
+    await minioClient.putObject('router', bucketName + '-' + objectName, buffer, buffer.length, {
+      'Content-Type': mediaType,
+    });
 
-//     console.log('')
-//     console.log('ROUTER • Arquivo salvo no MinIO:', 'router' + '/' + bucketName + '-' + objectName);
-//     console.log('')
+    console.log('')
+    console.log('ROUTER • Arquivo salvo no MinIO:', 'router' + '/' + bucketName + '-' + objectName);
+    console.log('')
 
-//     return objectName;
-//   } catch (error) {
-//     console.log('')
-//     console.error('ROUTER • Erro ao salvar arquivo no MinIO:', error);
-//     console.log('')
-//   }
-// }
+    return objectName;
+  } catch (error) {
+    console.log('')
+    console.error('ROUTER • Erro ao salvar arquivo no MinIO:', error);
+    console.log('')
+  }
+}
 
 fastify.get('/', async function handler (request, reply) {
   return 'i4 Router API'
@@ -126,7 +126,7 @@ fastify.post<{ Params: { numberId: string } }>('/:numberId', async function hand
       token: '39882bc13b5e520f417cf8acf7c2c4a2001', // 'BB270D0E17D7-4A17-AF19-534639FD982A',
       message: {
         mediaType: 'image',
-        media: url,
+        media: 'http://minio.i4ai.com.br/api/v1/download-shared-object/aHR0cHM6Ly9zMy5pNGFpLmNvbS5ici9yb3V0ZXIvNTUxMTk4NDM3OTkwNi0wN2UzM2UwZS0zYjdjLTRkYmEtOGU3ZS00M2I2ZGNmMTkxYWY_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1YVkRIWDdISklBSjBLUUdCOTBZMiUyRjIwMjQxMTEwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTExMFQyMzEzMTJaJlgtQW16LUV4cGlyZXM9NDMyMDAmWC1BbXotU2VjdXJpdHktVG9rZW49ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmhZMk5sYzNOTFpYa2lPaUpZVmtSSVdEZElTa2xCU2pCTFVVZENPVEJaTWlJc0ltVjRjQ0k2TVRjek1UTXlNelV4Tml3aWNHRnlaVzUwSWpvaVlXUnRhVzRpZlEuaTBDWDdLRmZveEJpaUxiNy1JOVFiYnFGZXVld3Z4eXRhcXIyd3NfTnM2ajhqcVJMV1oxbE1FSlFRNXVFMTY1bXpMT3hXRWhvaTBxWUVuTkxXcDZPbWcmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JnZlcnNpb25JZD1udWxsJlgtQW16LVNpZ25hdHVyZT0zNGJmMmI3OGY0NGFiYTAyMDgyMGU4OWQzMjA2MDM5MmQ1ZTA5OWEwNTQ0ZmYxZjI2MGQ1ZDViODlmNzQ0MTlk' //url,
       },
       messageType: MessageTypeEnum.ImageMessage,
     });
